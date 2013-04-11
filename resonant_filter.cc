f954742e51a0af43cfe78bafbaaa5f118c76391e
Vermeille <Guillaume.V.Sanchez@gmail.com>
2013-04-11 02:31:09 +0200

    filter: add two resonant filters

---
+++ resonant_filter.cc
@@ -0,0 +1,67 @@
+/**
+** @file resonant_filter.cc
+*/
+
+#include "resonant_filter.hh"
+
+#include <iostream>
+#include <cmath>
+
+#include "params.hh"
+
+ResonantFilter1::ResonantFilter1(MonoSource* src)
+    : src_(src),
+      buf_(3, 0)
+{
+    MkLowPass(800, 0.2);
+}
+
+void ResonantFilter1::MkLowPass(float freq, float q)
+{
+    a_[0] = -2. * q * cos(M_TAU * freq / 44100.);
+    a_[1] = q * q;
+
+    b_[0] = (1. - q * q) / 2.;
+    b_[1] = 0;
+    b_[2] = -b_[0];
+}
+
+void ResonantFilter1::MkHighPass(float freq)
+{
+}
+
+float ResonantFilter1::Gen()
+{
+    double v = src_->Gen();
+    buf_.push_front(v - a_[0] * buf_[0] - a_[1] * buf_[1]);
+    v = b_[0] * buf_[0] + b_[1] * buf_[1] + b_[2] * buf_[2];
+    //std::cerr << v << " ";
+    return v;
+}
+
+ResonantFilter2::ResonantFilter2(MonoSource* src)
+    : src_(src),
+      buf_(2, 0)
+{
+    MkLowPass(800, 0.2);
+}
+
+void ResonantFilter2::MkLowPass(float freq, float q)
+{
+    r_ = q;//1 - M_TAUOVR2 * (20000. / 44100.);
+    b_[0] = -(4 * r_ * r_ / (1. + r_ * r_)) * cos(M_TAU * freq / 44100.);
+    b_[1] = r_ * r_;
+}
+
+void ResonantFilter2::MkHighPass(float freq)
+{
+}
+
+float ResonantFilter2::Gen()
+{
+    double v = src_->Gen();
+    v = v - b_[0] * buf_[0] - b_[1] * buf_[1];
+    buf_.push_front(v);
+    return v;
+}
+
---
+++ resonant_filter.hh
@@ -0,0 +1,43 @@
+#pragma once
+
+#include <vector>
+
+#include "circular.hh"
+#include "mono_source.hh"
+
+/*
+** http://www.music.mcgill.ca/~gary/307/week2/filters.html#SECTION00018000000000000000
+*/
+
+class ResonantFilter1 : public MonoSource
+{
+    public:
+        ResonantFilter1(MonoSource* src);
+        virtual float Gen() override;
+
+        void MkLowPass(float freq, float q);
+        void MkHighPass(float freq);
+
+    private:
+        MonoSource* src_;
+        CircularBuffer<float> buf_;
+        double a_[2];
+        double b_[3];
+};
+
+class ResonantFilter2 : public MonoSource
+{
+    public:
+        ResonantFilter2(MonoSource* src);
+        virtual float Gen() override;
+
+        void MkLowPass(float freq, float q);
+        void MkHighPass(float freq);
+
+    private:
+        MonoSource* src_;
+        CircularBuffer<float> buf_;
+        double b_[2];
+        double r_;
+};
+
